0f31becf28627a44d09dd6cf80d1d567db8699d8
Raghava Seru <raghavamsc@gmail.com>
2021-10-11 16:08:09 -0400

    practice2 commit

--- cypress/tests/sample_main.js
+++ cypress/tests/sample_main.js
@@ -1,8 +1,5 @@
 const cypress = require("cypress");
-<<<<<<< HEAD
 
 cypress.console.log("my  js file from practice2 branch..!");
 cypress.console.log("my  js file from practice2 branch..! change 2");
-=======
 cypress.console.log("my  js file from main..!");
->>>>>>> 361c9e78142c1fad6307dd3bbc945bfda1292a66
